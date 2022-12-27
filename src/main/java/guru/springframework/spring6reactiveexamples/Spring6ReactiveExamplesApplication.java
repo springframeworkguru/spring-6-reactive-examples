4f0a1c8571eb228484c3cebdf7d0405cfeaf51de
John Thompson <john@springframework.guru>
2022-12-27 13:21:47 -0500

    Initial project add

---
+++ src/main/java/guru/springframework/spring6reactiveexamples/Spring6ReactiveExamplesApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6reactiveexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6ReactiveExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6ReactiveExamplesApplication.class, args);
+    }
+
+}
