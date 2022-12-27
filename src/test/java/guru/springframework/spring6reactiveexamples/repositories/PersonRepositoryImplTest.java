fa511ad2100004d2c712f33199253753e0886ebf
John Thompson <john@springframework.guru>
2022-12-27 16:07:30 -0500

    reverting changes

--- src/test/java/guru/springframework/spring6reactiveexamples/repositories/PersonRepositoryImplTest.java
+++ src/test/java/guru/springframework/spring6reactiveexamples/repositories/PersonRepositoryImplTest.java
@@ -14,20 +14,6 @@ class PersonRepositoryImplTest {
     PersonRepository personRepository = new PersonRepositoryImpl();
 
     @Test
-    void testGetByIdFound() {
-        Mono<Person> personMono = personRepository.getById(3);
-
-        assertTrue(personMono.hasElement().block());
-    }
-
-    @Test
-    void testGetByIdNotFound() {
-        Mono<Person> personMono = personRepository.getById(6);
-
-        assertFalse(personMono.hasElement().block());
-    }
-
-    @Test
     void testMonoByIdBlock() {
         Mono<Person> personMono = personRepository.getById(1);
 
